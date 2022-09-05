e6d86d0c19ef7c08ccad3e97fb0bc03cb32444e4
Polina <polina9130@gmail.com>
2022-09-05 15:48:35 +0300

    add objects decisions

---
+++ JS Advanced/Objects & Composition - Lab/01.City Record.js
@@ -0,0 +1,10 @@
+function solve(name, population, treasury) {
+    let city = {name, population, treasury};
+
+    console.log(city);
+}
+
+solve('Tortuga',
+7000,
+15000
+);
---
+++ JS Advanced/Objects & Composition - Lab/02.Town Population.js
@@ -0,0 +1,24 @@
+function solve(input) {
+    let towns = {};
+    for (const info of input) {
+        let infoArray = info.split(' <-> ');
+        let town = infoArray[0];
+        let population = Number(infoArray[1]);
+
+        if(!towns[town]){
+            towns[town] = 0;
+        }
+        towns[town] += population;
+    }
+
+    for (const key in towns) {
+        console.log(`${key}: ${towns[key]}`);
+    }
+}
+
+solve(['Istanbul <-> 100000',
+'Honk Kong <-> 2100004',
+'Jerusalem <-> 2352344',
+'Mexico City <-> 23401925',
+'Istanbul <-> 1000']
+);
