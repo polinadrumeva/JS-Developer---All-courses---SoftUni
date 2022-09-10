c457cefb5a25202e32f6c0acedf5780b7c5f0a36
Polinadrumeva <polina9230@gmail.com>
2022-09-10 22:31:42 +0300

    add

--- JS Advanced/DOM Introduction - Lab/05.Colorize Table/ColorizeTable.js
+++ JS Advanced/DOM Introduction - Lab/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,7 @@
 function colorize() {
-    // TODO
+    let rowElement = document.querySelectorAll('tr:nth-of-type(2n) td');
+
+    rowElement.forEach(x => {
+        x.style.backgroundColor = 'teal';
+    });
 }
--- JS Advanced/DOM Introduction - Lab/06.Sum Table/SumTable.js
+++ JS Advanced/DOM Introduction - Lab/06.Sum Table/SumTable.js
@@ -1,3 +1,12 @@
 function sumTable() {
 
+    let priceElement = document.querySelectorAll('tr td:nth-of-type(2)');
+
+    let sum = Array.from(priceElement).reduce((a,x) => {
+        let currentSum = Number(x.textContent || 0 );
+        return a + currentSum;
+    }, 0);
+
+    let resultElement = document.getElementById('sum');
+    resultElement.textContent = sum;
 }
