5b38fd5dd009356038a54e69e81e6d21f328b104
Polina <polina9130@gmail.com>
2022-09-14 09:56:45 +0300

    add words uppercase

--- JS Advanced/Syntax, Functions and Statements - Exercise/09.Words Uppercase.js
+++ JS Advanced/Syntax, Functions and Statements - Exercise/09.Words Uppercase.js
@@ -1,7 +1,23 @@
 function search(input) {
     let pattern = /([A-Za-z]+)/g;
     let words = [...input.matchAll(pattern)];
+    let upperWords = '';
    
+    for(let i = 0; i < words.length; i++) {
+       if (words.length == 1) {
+        upperWords += `${words[i][0]} `;
+       } else {
+        if (i == words.length - 1) {
+            upperWords += `${words[i][0]} `;
+        } else {
+            upperWords += `${words[i][0]}, `;
+        }
+       }
+    }
+
+    upperWords = upperWords.toUpperCase();
+    console.log(upperWords);
 }
 
 search('Hi, how are you?');
+search('hello');
