02c6ad306018ca174fd3aa0f8d65fd0ea5211b56
Polina <polina9130@gmail.com>
2022-09-15 16:46:36 +0300

    add list of items and delete from table

--- JS Advanced/DOM Manipulations and Events - Lab/01. List-Of-Items/app.js
+++ JS Advanced/DOM Manipulations and Events - Lab/01. List-Of-Items/app.js
@@ -1,3 +1,9 @@
 function addItem() {
-    console.log('TODO:...');
+    let itemsElement = document.getElementById('items');
+    let inputElement = document.getElementById('newItemText');
+    let liElement = document.createElement('li');
+
+    liElement.textContent = inputElement.value;
+
+    itemsElement.appendChild(liElement);
 }
--- JS Advanced/DOM Manipulations and Events - Lab/02. Delete-From-Table/app.js
+++ JS Advanced/DOM Manipulations and Events - Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,16 @@
 function deleteByEmail() {
-    console.log('TODO:...');
-}
+    let emailElement = document.querySelector('input[name="email"]');
+    let cellsElements = document.querySelectorAll('tr td:nth-of-type(2)');
+    let resultElement = document.getElementById('result');
+
+    let emailElements = Array.from(cellsElements);
+    let searchedElement = emailElements.find(x => x.textContent == emailElement.value);
+
+    if (searchedElement) {
+        searchedElement.parentNode.remove();
+        resultElement.textContent = 'Deleted.';
+    } else {
+        resultElement.textContent = 'Not found.';
+    }
+    
+} 
