d9e9555ca8e51a60a7e9fe525e7c7a920f12bbde
Polina <polina9130@gmail.com>
2022-09-19 14:24:54 +0300

    add magic matrices

--- JS Advanced/Arrays and Nested Arrays - Exercise/09.Magic Matrices.js
+++ JS Advanced/Arrays and Nested Arrays - Exercise/09.Magic Matrices.js
@@ -1,6 +1,7 @@
 function magic(input) {
     let sum = 0;
     let rowSum = 0;
+    let colSum = 0;
     let length = input.length;
     let isMagic = false;
     for(let i = 0; i < input.length; i++) {
@@ -10,12 +11,18 @@ function magic(input) {
         }
     }
 
+      for(let j = 0; j < input.length; j++) {
+            colSum += input[1][j];
+
+        }
+    
+
     for(let k = 0; k < input[0].length; k++) {
         rowSum += input[0][k];
 
     }
 
-    if (sum / length == rowSum) {
+    if (sum / length == rowSum && sum / length == colSum) {
         isMagic = true;
     }
 
