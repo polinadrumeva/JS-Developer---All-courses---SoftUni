4c311f00a136c0e96a4beb6a1acd792dda9d1e2c
Polina <polina9130@gmail.com>
2022-09-21 17:22:16 +0300

    add construction crew

--- JS Advanced/Objects & Composition - Exercise/02.Construction Crew.js
+++ JS Advanced/Objects & Composition - Exercise/02.Construction Crew.js
@@ -1,3 +1,28 @@
 function construction(input) {
-    
+    let obj = input;
+    if(obj.dizziness) {
+        let sum = 0.1 * obj.weight * obj.experience;
+        obj.levelOfHydrated += sum;
+        obj.dizziness = false;
+    }
+
+ return obj;
 }
+
+construction({ weight: 80,
+    experience: 1,
+    levelOfHydrated: 0,
+    dizziness: true }
+  );
+
+construction({ weight: 120,
+    experience: 20,
+    levelOfHydrated: 200,
+    dizziness: true }
+    );
+
+construction({ weight: 95,
+    experience: 3,
+    levelOfHydrated: 0,
+    dizziness: false }
+);
---
+++ JS Advanced/Objects & Composition - Exercise/07.Towns to JSON.js
@@ -0,0 +1,13 @@
+function convert(input) {
+    let 
+}
+
+convert(['| Town | Latitude | Longitude |',
+'| Sofia | 42.696552 | 23.32601 |',
+'| Beijing | 39.913818 | 116.363625 |']
+);
+
+convert()['| Town | Latitude | Longitude |',
+'| Veliko Turnovo | 43.0757 | 25.6172 |',
+'| Monatevideo | 34.50 | 56.11 |']
+;
