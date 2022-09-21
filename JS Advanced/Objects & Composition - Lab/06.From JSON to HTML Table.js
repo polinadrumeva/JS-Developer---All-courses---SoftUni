d09dd263c442d87e1d774f81b45dfc70dbf9e609
Polina <polina9130@gmail.com>
2022-09-21 15:33:33 +0300

    add JSON to HTML table

---
+++ JS Advanced/Objects & Composition - Lab/06.From JSON to HTML Table.js
@@ -0,0 +1,44 @@
+function solve(input) {
+    let students = JSON.parse(input);
+    let htmlText = ['<table>'];
+    let objNames = students[0];
+  
+    htmlText.push(makeKeyRow(objNames));
+    students.forEach(obj => htmlText.push(makeValueRow(obj)));
+    htmlText.push('</table>');
+  
+    function makeKeyRow(arr) {
+      let keys = [];
+      Object.keys(arr).forEach(key => {
+        keys.push(`<th>${escapeHTML(key)}</th>`);
+      });
+      return ("<tr>" + keys.join('') + "</tr>");
+    }
+  
+    function makeValueRow(obj) {
+      let rows = [];
+      Object.values(obj).forEach(value => {
+        rows.push(`<td>${escapeHTML(value)}</td>`);
+      });
+      return ("<tr>" + rows.join('') + "</tr>");
+    }
+  
+    function escapeHTML(value) {
+      return value
+        .toString()
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#39;');
+    }
+  
+    return htmlText.join('\r\n');
+}
+
+
+solve(`[{"Name":"Stamat",
+"Score":5.5},
+{"Name":"Rumen",
+"Score":6}]`
+);
