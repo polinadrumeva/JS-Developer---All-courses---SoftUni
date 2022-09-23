6be14bfe7fc19fcd63d471c85dd1adb99381e65e
Polina <polina9130@gmail.com>
2022-09-23 12:06:01 +0300

    add accordion

--- JS Advanced/DOM Introduction - Exercise/03. Accordion/accordion.js
+++ JS Advanced/DOM Introduction - Exercise/03. Accordion/accordion.js
@@ -1,3 +1,13 @@
 function toggle() {
-    console.log('TODO:...');
+   let textElement = document.getElementById('extra');
+   let buttonElement = document.getElementsByClassName('button')[0];
+
+   if(buttonElement.textContent == 'Less') {
+    textElement.style.display = 'none';
+    buttonElement.textContent = 'More'
+   }else if (buttonElement.textContent = 'More') {
+    textElement.style.display = 'block';
+    buttonElement.textContent = 'Less';
+   }
+   
 }
