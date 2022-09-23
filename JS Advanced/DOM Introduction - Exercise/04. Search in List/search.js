a9f541121f130c25142ffc8a842e0c6dec2a325b
Polina <polina9130@gmail.com>
2022-09-23 15:26:24 +0300

    add search in list

--- JS Advanced/DOM Introduction - Exercise/04. Search in List/search.js
+++ JS Advanced/DOM Introduction - Exercise/04. Search in List/search.js
@@ -1,3 +1,17 @@
 function search() {
-   // TODO
+   let townElements = document.getElementsByTagName('li');
+   let seachedElement = document.getElementById('searchText').value;
+   let numbersOfFound = 0;
+   let resultElement = document.getElementById('result');
+
+   for (const town of townElements) {
+      let townName = town.textContent;
+      if (seachedElement && townName.indexOf(seachedElement) >= 0) {
+         town.innerHTML = `<bold><u>${townName}</u></bold>`;
+         numbersOfFound++;
+      }
+   }
+
+   resultElement.textContent = `${numbersOfFound} matches found`;
+  
 }
