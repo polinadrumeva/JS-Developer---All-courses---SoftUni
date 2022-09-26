844d84bedbb0d77338e348ef56e59c616877316e
Polina <polina9130@gmail.com>
2022-09-26 17:11:54 +0300

    add generate report

--- JS Advanced/DOM Introduction - Exercise/08. Generate Report/report.js
+++ JS Advanced/DOM Introduction - Exercise/08. Generate Report/report.js
@@ -1,3 +1,21 @@
 function generateReport() {
-    //TODO
+    let tableRowElements = document.querySelectorAll("tbody tr");
+    let tableHeaderElements = document.querySelectorAll("thead tr th");
+    let result = [];
+
+    for(let i = 0; i < tableRowElements.length; i++) {
+        let tableData = tableRowElements[i].cells;
+        let tempData = {};
+        for(let k = 0; k < tableData.length; k++) {
+            let infoTHeader = tableHeaderElements[k].childNodes;
+            if (infoTHeader[1].checked === true) {
+                tempData[infoTHeader[0].textContent.trim().toLocaleLowerCase()] = tableData[k].textContent;
+            }
+        }
+
+        result.push(tempData);
+    }
+
+    let resultJSON = JSON.stringify(result);
+    document.getElementById("output").textContent = resultJSON;
 }
