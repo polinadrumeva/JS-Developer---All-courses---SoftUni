9c035d6cdbc0e3aed0b3149eae99c71a5c45cc3d
Polina <polina9130@gmail.com>
2022-09-26 14:13:03 +0300

    add

--- JS Advanced/DOM Introduction - Exercise/05. Table - Search Engine/solution.js
+++ JS Advanced/DOM Introduction - Exercise/05. Table - Search Engine/solution.js
@@ -2,7 +2,17 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      let searchedInput = document.getElementById("searchField").value;
+
+      let tableData = document.getElementsByTagName("tbody");
+
+      for (const row of tableData) {
+         let currentRow = row.textContent;
+         if (searchedInput && currentRow.indexOf(searchedInput) >= 0) {
+            row.style.background = 'yellow';
+         }
+      }
+   
 
    }
 }
--- JS Advanced/DOM Introduction - Exercise/06. Format the Text/solution.js
+++ JS Advanced/DOM Introduction - Exercise/06. Format the Text/solution.js
@@ -1,3 +1,17 @@
 function solve() {
-  //TODO
+  let textAreaElement = document.getElementById("input").value;
+  let text = textAreaElement.split(". ");
+  let resultElement = document.getElementById("output");
+
+  for(let i = 0; i < text.length; i+=3) {
+    let result = [];
+    for(let k = 0; k < 3; k++) {
+      if(text[i + k]) {
+        result.push(text[i+k]);
+      }
+    }
+
+    let resultText = result.join(". ").trim();  
+    resultElement.innerHTML += `<p>${resultText}</p>`;
+  }
 }
