02c1237b4bc93249502a18a059c994cb7db1605b
Polina <polina9130@gmail.com>
2022-09-27 16:32:07 +0300

    Complete task 03.Add/Delete

--- JS Advanced/DOM Manipulations and Events - Lab/03. Add-Delete/app.js
+++ JS Advanced/DOM Manipulations and Events - Lab/03. Add-Delete/app.js
@@ -1,3 +1,25 @@
 function addItem() {
-    //TODO...
+    let newElement = document.getElementById("newItemText").value;
+    let listElement = document.getElementById("items");
+
+    if(newElement.length === 0) {
+        return;
+    }
+
+    let listItemsElement = document.createElement("li");
+    listItemsElement.textContent = newElement;
+
+    let removeElement = document.createElement("a");
+    let linkTextElement = document.createTextNode("[Delete]");
+    removeElement.appendChild(linkTextElement);
+    removeElement.href = "#";
+    removeElement.addEventListener("click", deleteItem);
+
+    listItemsElement.appendChild(removeElement);
+    listElement.appendChild(listItemsElement);
+
+    function deleteItem() {
+        listItemsElement.remove();
+    }
+
 }
