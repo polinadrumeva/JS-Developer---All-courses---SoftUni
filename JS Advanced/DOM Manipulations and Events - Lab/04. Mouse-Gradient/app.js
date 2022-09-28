cc5e22155f35c8323e12fc9425278ad3e98446b8
Polinadrumeva <polina9230@gmail.com>
2022-09-28 23:48:25 +0300

    complete 05

--- JS Advanced/DOM Manipulations and Events - Lab/04. Mouse-Gradient/app.js
+++ JS Advanced/DOM Manipulations and Events - Lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,22 @@
+//First option - work & 100% Judge
+function attachGradientEvents() {
+    let gradient = document.getElementById('gradient');
+    
+    gradient.addEventListener('mousemove', gradientMove);
+    gradient.addEventListener('mouseout', gradientOut);
+ 
+    function gradientMove(event) {
+        let power = event.offsetX / (event.target.clientWidth - 1);
+        power = Math.trunc(power * 100);
+        document.getElementById('result').textContent = power + "%";
+    }
+ 
+    function gradientOut(event) {
+        document.getElementById('result').textContent = "";
+    }
+};
+
+// Second option - work & Judge doesn't like it
 function attachGradientEvents() {
     let gradientElement = document.getElementById('gradient');
 
@@ -11,3 +30,4 @@ function attachGradientEvents() {
         document.getElementById('result').textContent = result;
     }
 }
+
--- JS Advanced/DOM Manipulations and Events - Lab/05. Highlight-Active/app.js
+++ JS Advanced/DOM Manipulations and Events - Lab/05. Highlight-Active/app.js
@@ -1,3 +1,15 @@
 function focused() {
-    console.log('TODO:...');
+    let inputsElement = document.getElementsByTagName('input');
+    for (let input of inputsElement) {
+        input.addEventListener('focus', focusSection);
+        input.addEventListener('blur', blurSection);
+    }
+
+    function focusSection(event) {
+        event.target.parentElement.classList.add('focused');
+    }
+
+    function blurSection(event) {
+        event.target.parentElement.classList.remove('focused');
+    }
 }
