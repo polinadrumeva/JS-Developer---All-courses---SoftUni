635b8524f82c37aab0a096e1c86833f06ebf3a51
Polinadrumeva <polina9230@gmail.com>
2022-09-28 23:39:22 +0300

    complete 04.

--- JS Advanced/DOM Manipulations and Events - Lab/04. Mouse-Gradient/app.js
+++ JS Advanced/DOM Manipulations and Events - Lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,13 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    let gradientElement = document.getElementById('gradient');
+
+    gradientElement.addEventListener('mousemove', move);
+
+    function move(event) {
+        let position = event.offsetX;
+        let grWigth = event.target.offsetWidth;
+
+        let result = `${Math.trunc(position/grWigth * 100)}%`;
+        document.getElementById('result').textContent = result;
+    }
 }
