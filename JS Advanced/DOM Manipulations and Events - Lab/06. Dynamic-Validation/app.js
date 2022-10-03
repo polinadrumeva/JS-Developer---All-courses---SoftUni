f1697976cf7511af38edc2cc7177fc1e2fea7651
Polina <polina9130@gmail.com>
2022-10-03 14:31:38 +0300

    complete 06

--- JS Advanced/DOM Manipulations and Events - Lab/06. Dynamic-Validation/app.js
+++ JS Advanced/DOM Manipulations and Events - Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,17 @@
 function validate() {
-    console.log('TODO:...');
+    let emailElement = document.getElementById('email');
+    let emailElementValue = emailElement.value;
+    let pattern = /([a-z]+)@([a-z]+).([a-z]+)/;
+
+    emailElement.addEventListener('change', checkEmail);
+
+    function checkEmail(event) {
+        if(pattern.test(event.target.value)) {
+            event.target.classList.remove('error');
+            return;
+        }
+         
+         event.target.classList.add('error');
+    }
+    
 }
