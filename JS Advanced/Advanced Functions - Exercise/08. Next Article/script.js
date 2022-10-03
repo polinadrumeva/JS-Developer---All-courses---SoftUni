9b45ee3bfe71297fabc7d145ae1e3f8311d3cc54
Polina <polina9130@gmail.com>
2022-10-03 13:50:19 +0300

    complete 08

--- JS Advanced/Advanced Functions - Exercise/08. Next Article/script.js
+++ JS Advanced/Advanced Functions - Exercise/08. Next Article/script.js
@@ -1,3 +1,11 @@
 function getArticleGenerator(articles) {
-    // TODO
+    let myArticle = JSON.parse(JSON.stringify(articles));
+    let content = document.getElementById('content');
+    return () => {
+        if(!myArticle.length) {
+            return;
+        }
+        let current = myArticle.shift();
+        content.innerHTML += `<article>${current}</article>`;
+    }
 }
