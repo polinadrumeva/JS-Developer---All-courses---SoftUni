b438d6399baa212464198babf85f228d8a6bc027
Polina <polina9130@gmail.com>
2022-10-03 16:14:53 +0300

    complete 01

--- JS Advanced/DOM Manipulations and Events - Exercise/01. Sections/script.js
+++ JS Advanced/DOM Manipulations and Events - Exercise/01. Sections/script.js
@@ -1,3 +1,19 @@
 function create(words) {
-   console.log('TODO:...');
+   
+   for (let word of words) {
+      let divElement = document.createElement('div');
+      let pElement = document.createElement('p');
+      pElement.textContent = word;
+      pElement.style.display = 'none';
+      divElement.appendChild(pElement);
+
+      divElement.addEventListener('click', function() {
+         pElement.style.display = 'block';
+      });
+
+      let divContentElement = document.getElementById('content');
+      divContentElement.appendChild(divElement);
+
+   }
+
 }
