56cce8605f516ab7fc67fba86829c9c938e3f4c8
Polina <polina9130@gmail.com>
2022-10-03 11:59:19 +0300

    complete 07

--- JS Advanced/Advanced Functions - Exercise/07. Simple Calculator/app.js
+++ JS Advanced/Advanced Functions - Exercise/07. Simple Calculator/app.js
@@ -1,7 +1,30 @@
 function calculator() {
-    // TODO:
+    let selector1;
+    let selector2;
+    let resultSelector;
+    return {
+        init: (selector1, selector2, resultSelector) => {
+            selector1 = document.getElementById(selector1);
+            selector2 = document.getElementById(selector2);
+            resultSelector = document.getElementById(resultSelector);
+        }
+        add: () => {
+            let firstNum = Number(selector1.value);
+            let secondNum = Number(selector2.value);
+            let sum = firstNum + secondNum;
+            resultSelector.value = sum;
+        }
+        subtract: () => {
+            let firstNum = Number(selector1.value);
+            let secondNum = Number(selector2.value);
+            let sum = firstNum - secondNum;
+            resultSelector.value = sum;
+        }
+    }
 }
 
 
+const calculate = calculator (); 
+calculate.init ('#num1', '#num2', '#result'); 
 
 
