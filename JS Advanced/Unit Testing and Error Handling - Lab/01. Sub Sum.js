df1eabd03c0443b423efc6ece60727b059caf402
Polina <polina9130@gmail.com>
2022-10-04 14:59:42 +0300

    add 01 & 02

---
+++ JS Advanced/Unit Testing and Error Handling - Lab/01. Sub Sum.js
@@ -0,0 +1,25 @@
+function sum(array, startIndex, endIndex) {
+    let sum = 0;
+    if(!Array.isArray(array)) {
+        return 'NaN';
+    }
+    if(startIndex < 0) {
+        startIndex = 0;
+    }
+
+    if(endIndex > array.length - 1) {
+        endIndex = array.length - 1;
+    }
+
+    for(let i = startIndex; i <= endIndex; i++) {
+        sum += Number(array[i]);
+    }
+
+    return sum;
+}
+
+console.log(sum([10, 20, 30, 40, 50, 60], 3, 300));
+console.log(sum([1.1, 2.2, 3.3, 4.4, 5.5], -3, 1));
+console.log(sum([10, 'twenty', 30, 40], 0, 2));
+console.log(sum([], 1, 2));
+console.log(sum('text', 0, 2));
--- JS Advanced/Unit Testing and Error Handling - Lab/02. Playing Cards.js
+++ JS Advanced/Unit Testing and Error Handling - Lab/02. Playing Cards.js
@@ -19,27 +19,5 @@ function playing(face, suit) {
 
 
     playing('10', 'K').toString();
-    //playing('10', 'H').toString();
-    //playing('1', 'C').toString();
-
-
-
-function solve(face, suit) {
-    let validFaces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-    let suits = {
-        S: '\u2660',
-        H: '\u2665',
-        D: '\u2666',
-        C: '\u2663',
-    }
-    if (!validFaces.includes(face) || suits[suit] === 'undefined') {
-        throw new Error('Error');
-    }
- 
-    let card = {
-        face,
-        suit: suits[suit],
-        toString() { console.log(this.face + this.suit) }
-    }
-    return card;
-}
+    playing('10', 'H').toString();
+    playing('1', 'C').toString();
