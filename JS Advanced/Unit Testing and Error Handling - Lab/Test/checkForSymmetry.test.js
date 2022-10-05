d203d6c3793305c7d55a468d5cc2c6e8dfdafe6b
Polinadrumeva <polina9230@gmail.com>
2022-10-05 17:17:20 +0300

    add tests

---
+++ JS Advanced/Unit Testing and Error Handling - Lab/Test/checkForSymmetry.test.js
@@ -0,0 +1,22 @@
+const {expect} = require('chai');
+const { isSymmetric } = require('../05. Check for Symmetry');
+
+describe('checkSymmetry', () => {
+    it('should return false if is not an array', () => {
+        let array = 'string';
+        let result = isSymmetric(array);
+        expect(result).to.be.false;
+    });
+
+    it('should return false if arent equals', () => {
+        let array = [1,2,3];
+        let result = isSymmetric(array);
+        expect(result).to.be.false;
+    });
+
+    it('should return true if are equals', () => {
+        let array = [3,2,3];
+        let result = isSymmetric(array);
+        expect(result).to.be.true;
+    });
+});
