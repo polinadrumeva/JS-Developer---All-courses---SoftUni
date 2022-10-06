2230f2b6dad760ae8aabc75564690b2a33edbb46
Polina <polina9130@gmail.com>
2022-10-06 17:12:42 +0300

    complete 06

--- JS Advanced/Unit Testing and Error Handling - Exercise/06.Dynamic-Validation/app.js
+++ JS Advanced/Unit Testing and Error Handling - Exercise/06.Dynamic-Validation/app.js
@@ -1,3 +1,15 @@
 function validate() {
-    // TODO
+    let emailElement = document.getElementById("email");
+    
+    emailElement.addEventListener("change", checkValidate);
+    function checkValidate(event) {
+        let emailValueElement = event.target.value;
+        let validPattern = /[a-z]+@[a-z]+.[a-z]+/g;
+        if(validPattern.test(emailValueElement)) {
+            event.target.classList.remove('error');
+            
+        } else {
+            event.target.classList.add('error');
+        }
+    }
 }
