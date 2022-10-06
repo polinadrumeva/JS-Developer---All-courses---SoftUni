28d61639551d2c0492c47e906441e7993fc19730
Polina <polina9130@gmail.com>
2022-10-06 13:26:08 +0300

    add 01

---
+++ JS Advanced/Unit Testing and Error Handling - Exercise/01. Request Validator.js
@@ -0,0 +1,36 @@
+function requestValidator (obj) {
+    if(!obj.method || obj.method !== 'GET' && obj.method !== 'POST'&& obj.method !== 'DELETE' && obj.method !== 'CONNECT') {
+        throw new Error('Invalid request header: Invalid Method');
+    } else if (!obj.uri || obj.uri === '') {
+        throw new Error('Invalid request header: Invalid URI');
+    } else if (!obj.version && obj.version !== 'HTTP/0.9' && obj.version !== 'HTTP/1.0' && obj.version !== 'HTTP/1.1' && obj.version !== 'HTTP/2.0' ) {
+        throw new Error('Invalid request header: Invalid Version');
+    } else if (obj.message === undefined) {
+        throw new Error('Invalid request header: Invalid Message');
+    } else {
+        return obj;
+    }
+}
+
+console.log(requestValidator({
+    method: 'GET',
+    uri: 'svn.public.catalog',
+    version: 'HTTP/1.1',
+    message: ''
+  }
+  ));
+
+  console.log(requestValidator({
+    method: 'OPTIONS',
+    uri: 'git.master',
+    version: 'HTTP/1.1',
+    message: '-recursive'
+  }
+  ));
+
+  console.log(requestValidator({
+    method: 'POST',
+    uri: 'home.bash',
+    message: 'rm -rf /*'
+  }
+  ));
