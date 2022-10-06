b8cbc14685f402312105f2d266f9116747d77659
Polina <polina9130@gmail.com>
2022-10-06 13:39:53 +0300

    complete 02

---
+++ JS Advanced/Unit Testing and Error Handling - Exercise/Test/evenOrOdd.test.js
@@ -0,0 +1,24 @@
+const {expect} = require('chai');
+const { isOddOrEven } = require('../02. Even Or Odd');
+
+describe('isOddOrEven', () => {
+    it('should return undefined if not a string', () => {
+        expect(isOddOrEven(5)).to.be.undefined;
+    });
+
+    it('should return undefined for array if not a string', () => {
+        expect(isOddOrEven(['see', 'you'])).to.be.undefined;
+    });
+
+    it('should return even', () => {
+        expect(isOddOrEven('seen')).to.be.equal('even');
+    });
+
+    it('should return odd', () => {
+        expect(isOddOrEven('see')).to.be.equal('odd');
+    });
+
+    it('should return true information', () => {
+        expect(isOddOrEven('see you soon')).to.be.equal('even');
+    });
+})
