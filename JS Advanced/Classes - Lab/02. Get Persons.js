cbfdbc273c41487e5f3c31bca5d8bc1aeaceade0
Polina <polina9130@gmail.com>
2022-10-07 16:07:39 +0300

    complete 02

--- JS Advanced/Classes - Lab/02. Get Persons.js
+++ JS Advanced/Classes - Lab/02. Get Persons.js
@@ -1,4 +1,5 @@
-class Person {
+function getPersons() {
+    class Person {
     constructor(firstname, lastname, age, email) {
         this.firstName = firstname;
         this.lastName = lastname;
@@ -9,22 +10,16 @@ class Person {
 
     toString() {
         return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`;
-    }
-}
+    }}
 
-function returnName() {
-    let persons = [];
-    let person1 = new Person('Anna', 'Simpson', 22, 'anna@yahoo.com');
-    let person2 = new Person('SoftUni');
-    let person3 = new Person('Stephan', 'Johnson', 25);
-    let person4 = new Person('Gabriel', 'Peterson', 24, 'g.p@gmail.com');
-    persons.push(person1);
-    persons.push(person2);
-    persons.push(person3);
-    persons.push(person4);
+    return [
+        new Person('Anna', 'Simpson', 22, 'anna@yahoo.com'), 
+        new Person('SoftUni'),
+        new Person('Stephan', 'Johnson', 25),
+        new Person('Gabriel', 'Peterson', 24, 'g.p@gmail.com')
 
-    return persons;
+    ]
 }
 
 
-console.log(returnName());
+console.log(getPersons())
