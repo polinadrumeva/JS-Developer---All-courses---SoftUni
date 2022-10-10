53c82a777cd6f4696b82380b7418f7d8c264c895
Polina <polina9130@gmail.com>
2022-10-10 17:33:40 +0300

    add

--- JS Advanced/Unit Testing and Error Handling - Exercise/07.Form-Validation/formValidation.js
+++ JS Advanced/Unit Testing and Error Handling - Exercise/07.Form-Validation/formValidation.js
@@ -1,4 +1,19 @@
 function validate() {
     let usernameElement = document.getElementById('username');
-    let usernamePattern = //g;
+    let usernamePattern = /[A-z0-9]+/g;
+    let passwordElement = document.getElementById('password');
+    let confirmPasswordElement = document.getElementById('confirm-password');
+    let passwordPattern = /[A-z0-9_]+/g;
+    let emailElement = document.getElementById('email');
+    let emailPattern = /[A-z0-9@.]+/g;
+
+    let buttonElement = document.getElementById('submit');
+
+    buttonElement.addEventListener('click', clickMe);
+    function clickMe(event) {
+        if(usernameElement.value < 3 || usernameElement.value > 20) {
+            usernameElement.style.border = 'red';
+        } 
+    }
+
 }
