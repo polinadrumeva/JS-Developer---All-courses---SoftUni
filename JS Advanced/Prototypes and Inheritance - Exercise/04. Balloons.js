0b28b3dda561913c78b9575a4e13e93de29a45c1
Polina <polina9130@gmail.com>
2022-10-11 16:51:24 +0300

    complete 04

---
+++ JS Advanced/Prototypes and Inheritance - Exercise/04. Balloons.js
@@ -0,0 +1,44 @@
+function  solution() {
+    class Balloon {
+        constructor(color, hasWeight) {
+            this.color = color;
+            this.hasWeight = hasWeight;
+        }
+    }
+
+    class PartyBalloon extends Balloon {
+        constructor(color, hasWeight, ribbonColor, ribbonLegth) {
+            super(color, hasWeight)
+            this._ribbon = {color: ribbonColor, length: ribbonLegth}
+        }
+
+        get ribbon() {
+            return this._ribbon;
+        }
+    }
+
+    class BirthdayBalloon extends PartyBalloon {
+        constructor(color, hasWeight, ribbonColor, ribbonLegth, text) {
+            super(color, hasWeight, ribbonColor, ribbonLegth)
+            this.text = text;
+        }
+
+        get text() {
+            return this.text;
+        }
+    }
+
+    return {
+        Balloon: Balloon,
+        PartyBalloon: PartyBalloon, 
+        BirthdayBalloon: BirthdayBalloon
+    }
+}
+
+let classes = solution();
+let testBalloon = new classes.Balloon("yellow", 20.5);
+let testPartyBalloon = new classes.PartyBalloon("yellow", 20.5, "red", 10.25);
+let ribbon = testPartyBalloon.ribbon;
+console.log(testBalloon);
+console.log(testPartyBalloon);
+console.log(ribbon);
