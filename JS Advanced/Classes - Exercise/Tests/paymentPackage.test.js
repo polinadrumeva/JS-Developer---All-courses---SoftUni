f91cc6d5a2de41a1509e0bb9905c34e8b35ba4fc
Polina <polina9130@gmail.com>
2022-10-13 09:58:09 +0300

    complete 12

--- JS Advanced/Classes - Exercise/Tests/paymentPackage.test.js
+++ JS Advanced/Classes - Exercise/Tests/paymentPackage.test.js
@@ -36,7 +36,7 @@ describe("PaymentPackage", function() {
     it("should return correct vat2", function() {
         let package = new PaymentPackage("Tests", 10);
         
-        assert.equal(package.VAT = 20, 20);
+        assert.equal(package.VAT = 40, 40);
     });
 
     it("should throw exception if value is not a number", function() {
@@ -47,5 +47,28 @@ describe("PaymentPackage", function() {
     it("should throw exception if value is a negative number", function() {
         let package = new PaymentPackage("Tests", 10);
         assert.throw(() => package.VAT = -10, 'VAT must be a non-negative number');
+    });
+
+    it("should return correct active", function() {
+        let package = new PaymentPackage("Tests", 10);
+        assert.equal(package.active, true);
+    });
+
+    it("should return correct active2", function() {
+        let package = new PaymentPackage("Tests", 10);
+        
+        assert.equal(package.active = false, false);
+    });
+
+    it("should throw exception if value is not a boolean", function() {
+        let package = new PaymentPackage("Tests", 10);
+        assert.throw(() => package.active = "name", 'Active status must be a boolean');
+    });
+
+    it("should return correct toString", function() {
+        let package = new PaymentPackage("Tests", 10);
+        let actualString = package.toString();
+        let expectedString = `Package: Tests\n- Value (excl. VAT): 10\n- Value (VAT 20%): 12`;
+        assert.equal(actualString, expectedString);
     });
 });
