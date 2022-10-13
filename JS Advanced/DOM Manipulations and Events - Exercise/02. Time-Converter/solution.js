3021c205c6b84f10ff4f98ac9c37d4fa02c94920
Polina <polina9130@gmail.com>
2022-10-13 13:20:03 +0300

    complete 02

--- JS Advanced/DOM Manipulations and Events - Exercise/02. Time-Converter/solution.js
+++ JS Advanced/DOM Manipulations and Events - Exercise/02. Time-Converter/solution.js
@@ -1,5 +1,30 @@
 function attachEventsListeners() {
 
-    let buttonsElement = document.getElementsByTagName('input[type=button]');
+    let buttonsElements = Array.from(document.querySelectorAll('input[type=button]'));
+    for (let button of buttonsElements) {
+        button.addEventListener('click', convert);
+    }
+
+    function convert(event) {
+        let inputValueElement = Number(event.target.parentElement.querySelector('input[type=text]').value);
+        let unitElement = event.target.id;
+
+        switch(unitElement) {
+            case'daysBtn': calculate(inputValueElement); break;
+            case'hoursBtn':calculate(inputValueElement/24); break;
+            case'minutesBtn':calculate(inputValueElement/24/60); break;
+            case'secondsBtn':calculate(inputValueElement/24/60/60); break;
+        }
+    }
+
+    function calculate(inputValueElement) {
+        let inputsElements = Array.from(document.querySelectorAll('input[type=text]'));
+        inputsElements.shift().value = inputValueElement;
+        let currentValue = inputValueElement * 24;
+        for (let input of inputsElements) {
+            input.value = currentValue;
+            currentValue *= 60;
+        }
+    }
     
 }
