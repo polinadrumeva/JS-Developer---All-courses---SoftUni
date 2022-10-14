011b7fa2b26390318e3d9572605ef56a84535188
Polina <polina9130@gmail.com>
2022-10-14 17:16:09 +0300

    add

--- JS Advanced/Exams/JS Adv. Retake Exam-12.08.2021/01. Furniture Store/app.js
+++ JS Advanced/Exams/JS Adv. Retake Exam-12.08.2021/01. Furniture Store/app.js
@@ -1,27 +1,75 @@
 window.addEventListener('load', solve);
 
 function solve() {
-    let modelElement = document.getElementById("model");
-    let yearElement = document.getElementById("year");
-    let descriptionElement = document.getElementById("description");
-    let priceElement = document.getElementById("price");
-    document.getElementById('add').addEventListener('click', added);
-    let furnitureListElement = document.getElementById("furniture-list");
+    let model = document.querySelector('#model');
+    let year = document.querySelector('#year');
+    let description = document.querySelector('#description');
+    let price = document.querySelector('#price');
+    let addBtn = document.querySelector('#add');
 
-    function added(event) {
-        if(modelElement.value !== '' && yearElement.value !== '' && descriptionElement.value !== '' 
-        && priceElement.value >= 0 && Number(priceElement.value) > 0 && Number(yearElement.value) > 0) {
-            let trElement = document.createElement("tr");
-            trElement.classList.add("info");
-            furnitureListElement.appendChild(trElement);
-            let modelListElement = document.createElement("td");
-            modelListElement.textContent = modelElement.value;
-            trElement.appendChild(modelListElement);
-            let priceListElement = document.createElement("td");
-            priceListElement.textContent = Number(priceElement.value).toFixed(2);
-            trElement.appendChild(priceListElement);
+    let furnitureList = document.querySelector('#furniture-list');
+    let totalPrice = document.querySelector('.total-price');
+
+    addBtn.addEventListener('click', validateAndAdd);
+
+    function validateAndAdd(e) {
+        if (model.value !== '' && year.value !== '' && description.value !== '' && price.value >= 0 
+        && Number(price.value) > 0 && Number(year.value) > 0) {
+            add(e, model.value, year.value, description.value, price.value);
+            clearInputs();
         }
-
     }
-   
+
+    function add(e, model, year, description, price) {
+        e.preventDefault();
+
+        let firstTr = htmlGenerator('tr', '', furnitureList);
+        firstTr.setAttribute('class', 'info');
+        htmlGenerator('td', `${model}`, firstTr);
+        htmlGenerator('td', `${Number(price).toFixed(2)}`, firstTr);
+        let td = htmlGenerator('td', '', firstTr);
+
+        let moreBtn = htmlGenerator('button', 'More Info', td);
+        moreBtn.setAttribute('class', 'moreBtn');
+
+        let buyBtn = htmlGenerator('button', 'Buy it', td);
+        buyBtn.setAttribute('class', 'buyBtn');
+
+        let secondTr = htmlGenerator('tr', '', furnitureList);
+        secondTr.setAttribute('class', 'hide');
+        htmlGenerator('td', `Year: ${year}`, secondTr);
+        let descriptionTd = htmlGenerator('td', `Description: ${description}`, secondTr);
+        descriptionTd.setAttribute('colspan', 3);
+
+        moreBtn.addEventListener('click', (e) => {
+            e.target.textContent === 'More Info'
+                ? e.target.textContent = 'Less Info'
+                : e.target.textContent = 'More Info';
+            secondTr.style.display = 'none'
+                ? secondTr.style.display = 'contents'
+                : secondTr.style.display = 'none';
+        })
+
+        buyBtn.addEventListener('click', (e) => {
+            firstTr.remove();
+            secondTr.remove();
+            totalPrice.textContent = (Number(totalPrice.textContent) + Number(price)).toFixed(2);
+        })
+    }
+
+    function htmlGenerator(tagName, content, parent) {
+        let el = document.createElement(tagName);
+        el.textContent = content;
+        if (parent) {
+            parent.appendChild(el);
+        }
+        return el;
+    }
+
+    function clearInputs() {
+        model.value = '';
+        year.value = '';
+        description.value = '';
+        price.value = '';
+    }
 }
