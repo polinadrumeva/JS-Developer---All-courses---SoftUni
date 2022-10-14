0794ebe39764aacd2966f7349740f84c5b09f624
Polina <polina9130@gmail.com>
2022-10-14 15:40:02 +0300

    add

--- JS Advanced/Exams/JS Adv. Retake Exam-12.08.2021/01. Furniture Store/app.js
+++ JS Advanced/Exams/JS Adv. Retake Exam-12.08.2021/01. Furniture Store/app.js
@@ -1,5 +1,24 @@
 window.addEventListener('load', solve);
 
 function solve() {
-    //TO DO
+    let modelElement = document.getElementById("model");
+    let yearElement = document.getElementById("year");
+    let descriptionElement = document.getElementById("description");
+    let priceElement = document.getElementById("price");
+    document.getElementById('add').addEventListener('click', added);
+    let furnitureListElement = document.getElementById("furniture-list");
+
+    function added(event) {
+        let modelListElement = document.createElement("td");
+        modelListElement.textContent = modelElement.value;
+        let priceListElement = document.createElement("td");
+        priceListElement.textContent = priceElement.value.toFixed(2);
+        let trElement = document.createElement("tr");
+        trElement.classList.add("info");
+        trElement.appendChild(createCell(modelListElement));
+        trElement.appendChild(createCell(priceListElement));
+
+       furnitureListElement.appendChild(trElement);
+    }
+   
 }
