cf9459e0145658fce85673ee2d4102a4652f50fe
Polina <polina9130@gmail.com>
2022-10-18 17:30:35 +0300

    start

--- JS Advanced/Exams/JS Adv. Final Exam - 19.02.2022/01. Work Process/app.js
+++ JS Advanced/Exams/JS Adv. Final Exam - 19.02.2022/01. Work Process/app.js
@@ -1,4 +1,37 @@
 function solve() {
-    //TODO
+   let firstNameElement = document.getElementById("fname");
+   let lastNameElement = document.getElementById("lname");
+   let emailElement = document.getElementById("email");
+   let birthElement = document.getElementById("birth");
+   let positionElement = document.getElementById("position");
+   let salaryElement = document.getElementById("salary");
+   let tableElement = document.getElementById("tbody");
+   document.getElementById("add-worker").addEventListener('click', addedWorker);
+
+   function addedWorker(event) {
+        event.preventDefault();
+        if(!firstNameElement.value || !lastNameElement.value || !emailElement.value || !birthElement.value || !positionElement.value || !salaryElement.value) {
+            debugger;
+            tableElement.innerHTML += `<tr>
+                <td>`${firstNameElement.value}`</td>
+                <td>`${lastNameElement.value}`</td>
+                <td>`${emailElement.value}`</td>
+                <td>`${birthElement.value}`</td>
+                <td>`${positionElement.value}`</td>
+                <td>`${salaryElement.value}`</td>
+                <td><button class='fired'>Fired</button> <button class='edit'>Edit</button></td>
+                </tr>`;
+            clear(); 
+        }
+   }
+
+   function clear() {
+    firstNameElement.value = '';
+    lastNameElement.value = '';
+    emailElement.value = '';
+    birthElement.value = '';
+    positionElement.value = '';
+    salaryElement.value = '';
+   }
 }
 solve()
