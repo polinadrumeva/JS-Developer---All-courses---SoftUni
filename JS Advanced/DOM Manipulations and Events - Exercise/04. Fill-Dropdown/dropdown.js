64e786e9f596b8cbf843666734eb3fe5871877c5
Polina <polina9130@gmail.com>
2022-10-20 17:35:37 +0300

    update

--- JS Advanced/DOM Manipulations and Events - Exercise/04. Fill-Dropdown/dropdown.js
+++ JS Advanced/DOM Manipulations and Events - Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,14 @@
 function addItem() {
-    console.log('TODO:...');
+    let textInputElement = document.querySelector('#newItemText');
+    let valueInputElement = document.querySelector('#newItemValue');
+    let dropdownElement = document.querySelector('#menu');
+
+    let optionElement = document.createElement('option');
+    optionElement.text = textInputElement.value;
+    optionElement.value = valueInputElement.value;
+    dropdownElement.appendChild(optionElement);
+    
+    textInputElement.value = '';
+    valueInputElement.value = '';
+
 }
