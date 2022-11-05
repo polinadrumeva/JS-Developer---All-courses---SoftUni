53d6f7969b48a8a94d84cd397cb2bf5ae79f7f68
Polinadrumeva <polina9230@gmail.com>
2022-11-05 13:35:51 +0200

    complete 03

--- JS Applications/Asynchronous Programming - Lab/03.Gighub-Commits/app.js
+++ JS Applications/Asynchronous Programming - Lab/03.Gighub-Commits/app.js
@@ -1,4 +1,23 @@
-function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+async function loadCommits() {
+    const username = document.getElementById('username').value;
+    const repoName = document.getElementById('repo').value;
+    const list = document.getElementById('commits');
+
+    try{
+        const response = await fetch(`https://api.github.com/repos/${username}/${repoName}/commits`);
+	    if(response.ok == false) {
+		    throw new Error(`Error: ${response.status} (Not Found))`);
+	    }
+
+        let data = await response.json();
+        for (let repo of data) {
+            let item = document.createElement('li');
+            item.textContent = `${repo.commit.author.name}: ${repo.commit.message}`;
+            list.appendChild(item);
+        }
+    } catch {
+        const list = document.getElementById('commits');
+        list.textContent = error.message;
+    }
 }
+
