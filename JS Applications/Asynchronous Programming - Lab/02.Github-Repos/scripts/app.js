41604ff8675f3f9615d4c68e04458427ce33ce5b
Polinadrumeva <polina9230@gmail.com>
2022-11-05 11:30:41 +0200

    complete 02

--- JS Applications/Asynchronous Programming - Lab/02.Github-Repos/scripts/app.js
+++ JS Applications/Asynchronous Programming - Lab/02.Github-Repos/scripts/app.js
@@ -1,3 +1,37 @@
 function loadRepos() {
-	console.log("TODO...");
+	let usernameElement = document.getElementById('username').value;
+	fetch(`https://api.github.com/users/${usernameElement}/repos`)
+	.then(handleResponse)
+	.then(handleData)
+	.catch(handleErrors);
+}
+
+function handleResponse(response){
+	if(response.ok == false) {
+		throw new Error(`Error: ${response.status} ${response.statusText}`);
+	}
+
+	return response.json();
+}
+
+function handleData(data) {
+	const list = document.getElementById('repos');
+
+	const items = data.map(repo =>  {
+		const liElement = document.createElement('li');
+		const aElement = document.createElement('a');
+		aElement.href = repo.html_url;
+		aElement.textContent = repo.full_name;
+		liElement.appendChild(aElement);
+
+		return liElement;
+	});
+
+	list.replaceChildren(...items);
+
+}
+
+function handleErrors(error) {
+	const list = document.getElementById('repos');
+	list.textContent = error.message;
 }
