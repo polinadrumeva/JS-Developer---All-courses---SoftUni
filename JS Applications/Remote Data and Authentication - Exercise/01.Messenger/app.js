542989298a93b03839b56dd0ad8bf90391734b38
Polina <polina9130@gmail.com>
2022-11-08 17:10:06 +0200

    start 01

--- JS Applications/Remote Data and Authentication - Exercise/01.Messenger/app.js
+++ JS Applications/Remote Data and Authentication - Exercise/01.Messenger/app.js
@@ -1,5 +1,34 @@
 function attachEvents() {
-    console.log('TODO...');
+    document.getElementById('submit').addEventListener('click', onPost);
+    document.getElementById('refresh').addEventListener('click', reloadAll);
+
+    async function onPost() {
+        const name = document.getElementsByName('author').values[0];
+        const content = document.getElementsByName('content').values[0];
+
+        await postComment({name, content});
+    }
+}
+
+async function getComments() {
+    const response = await fetch('http://localhost:3030/jsonstore/messenger');
+    const data = response.json();
+
+    return Object.values(data);
+}
+
+async function postComment(comment) {
+    const response = await fetch('http://localhost:3030/jsonstore/messenger', {
+        method: 'post', 
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(comment)
+    });
+
+    const data = await response.json();
+    return data;
+
 }
 
 attachEvents();
