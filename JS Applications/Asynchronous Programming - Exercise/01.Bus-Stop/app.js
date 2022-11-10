84cda4409cfe5d3bec1c40ba11f77a578ad58970
Polina <polina9130@gmail.com>
2022-11-10 14:23:01 +0200

    complete 01

--- JS Applications/Asynchronous Programming - Exercise/01.Bus-Stop/app.js
+++ JS Applications/Asynchronous Programming - Exercise/01.Bus-Stop/app.js
@@ -1,3 +1,24 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+    const busElement = document.getElementById('stopId');
+    const bus = busElement.value;
+    const url = `http://localhost:3030/jsonstore/bus/businfo/${bus}`; 
+    const stopName = document.getElementById('stopName');
+    const list = document.getElementById('buses');
+    list.innerHTML = "";
+    bus.textContent = "";
+
+    try{
+        const response = await fetch(url);
+    const data = await response.json();
+
+    stopName.textContent = data.name;
+    Object.entries(data.buses).forEach(([k,v]) => {
+        const liElement = document.createElement('li');
+        liElement.textContent = `Bus ${k} arrives in ${v} minutes`;
+        list.appendChild(liElement);
+    });
+    } catch(error) {
+        stopName.textContent = "Error";
+    }
+    
 }
