019505d7c7c18c33242330c0cb06ef96e153da6b
Polina <polina9130@gmail.com>
2022-11-11 16:22:23 +0200

    complete 50/100

--- JS Applications/Asynchronous Programming - Exercise/06.Blog/app.js
+++ JS Applications/Asynchronous Programming - Exercise/06.Blog/app.js
@@ -7,6 +7,7 @@ function attachEvents() {
 async function loadPosts() {
     const selectOp = document.getElementById('posts');
     const url = `http://localhost:3030/jsonstore/blog/posts`;
+    selectOp.innerHTML = '';
     const response = await fetch(url);
     const data = await response.json();
 
