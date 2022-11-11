485284186a5e2c381e36b4b84924ac3124c803bb
Polina <polina9130@gmail.com>
2022-11-11 14:53:54 +0200

    complete 06 50/100

--- JS Applications/Asynchronous Programming - Exercise/06.Blog/app.js
+++ JS Applications/Asynchronous Programming - Exercise/06.Blog/app.js
@@ -1,5 +1,50 @@
 function attachEvents() {
-    console.log('TODO...');
+    document.getElementById('btnLoadPosts').addEventListener('click', loadPosts);
+    document.getElementById('btnViewPost').addEventListener('click', viewPosts);
+
+}
+
+async function loadPosts() {
+    const selectOp = document.getElementById('posts');
+    const url = `http://localhost:3030/jsonstore/blog/posts`;
+    const response = await fetch(url);
+    const data = await response.json();
+
+    Object.values(data).forEach(post => {
+        const option = document.createElement('option');
+        option.value = post.id;
+        option.textContent = post.title;
+        selectOp.appendChild(option);
+    })
+}
+
+async function viewPosts() {
+    const postUrl = `http://localhost:3030/jsonstore/blog/posts`;
+    const commentsUrl = `http://localhost:3030/jsonstore/blog/comments`;
+    
+    const selectOp = document.getElementById('posts').selectedOptions[0].value;
+    const title = document.getElementById('post-title');
+    const postBody = document.getElementById('post-body');
+    const postCommnets = document.getElementById('post-comments');
+    postCommnets.innerHTML = '';
+    
+    const postResponce = await fetch(postUrl);
+    const postData = await postResponce.json();
+
+    const commentsResponce = await fetch(commentsUrl);
+    const commentsData = await commentsResponce.json();
+
+    const selectedPost = Object.values(postData).find(x => x.id == selectOp);
+    title.textContent = selectedPost.title;
+    postBody.textContent = selectedPost.body;
+
+    const comments = Object.values(commentsData).filter(x => x.postId == selectOp);
+    comments.forEach(x=> {
+        const li = document.createElement('li');
+        li.textContent = x.text;
+        postCommnets.appendChild(li);
+    });
+
 }
 
 attachEvents();
