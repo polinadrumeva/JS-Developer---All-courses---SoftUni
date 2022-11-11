2bd4e14f4b9e48c12f04597bf30ac48a03f7356e
Polina <polina9130@gmail.com>
2022-11-11 13:26:41 +0200

    complete 03

--- JS Applications/Asynchronous Programming - Exercise/03.Forecaster/app.js
+++ JS Applications/Asynchronous Programming - Exercise/03.Forecaster/app.js
@@ -1,37 +1,88 @@
 function attachEvents() {
-   document.getElementById('submit').addEventListener('click', getWeather);
-}
+    const baseUrl = 'http://localhost:3030/jsonstore/forecaster';
+    const forecastDiv = document.querySelector('#forecast');
+    const currentDiv = document.querySelector('#current');
+    const upcomingDiv = document.querySelector('#upcoming');
+    let todaysDiv = createTag('div', null, 'forecasts');
+    let threeDayForecast = createTag('div', null, 'forecast-info');
 
-async function getWeather() {
-    const url = `http://localhost:3030/jsonstore/forecaster/locations`;
-    const response = await fetch(url);
-    const data = await response.json();
+    const symbols = {
+        'Sunny': '☀',
+        'Partly sunny': '⛅',
+        'Overcast': '☁',
+        'Rain': '☂',
+        'Degrees': '°'
 
-    let townElement = document.getElementById('location').value;
-    const infoTown = data.find(x => x.name === townElement);
-    createForecast(infoTown.code)
-}
+    }
+    const submitButton = document.querySelector('#submit');
+    submitButton.addEventListener('click', onClick);
 
-async function createForecast(code) {
-    const urlToday = `http://localhost:3030/jsonstore/forecaster/today/${code}`;
-    const url3days = `http://localhost:3030/jsonstore/forecaster/upcoming/${code}`;
+    async function onClick() {
+        todaysDiv.innerHTML = '';
+        threeDayForecast.innerHTML = '';
+        let location = document.querySelector('#location').value;
 
-    const responseToday = await fetch(urlToday);
-    const dataToday = await responseToday.json();
+        try {
 
-    const response3days = await fetch(url3days);
-    const data3days = await response3days.json();
+            let result = await fetch(`${baseUrl}/locations`);
+            let data = await result.json();
 
-    createToday(dataToday);
-    createUncoming(data3days);
-}
+            let searched = data.find(l => l.name == location);
 
-function createToday(data) {
+            let todayForecast = await fetch(`${baseUrl}/today/${searched.code}`);
+            let todaysData = await todayForecast.json();
 
-}
+            let upcoming = await fetch(`${baseUrl}/upcoming/${searched.code}`);
+            let upcomingData = await upcoming.json();
 
-function createUncoming(data) {
-    
+            displayTodaysData(todaysData);
+            displayUpcomingData(upcomingData);
+
+
+        } catch (error) {
+            todaysDiv.innerHTML = '';
+            threeDayForecast.innerHTML = '';
+            forecastDiv.style.display = 'block';
+            let errorSpan = document.createElement('span');
+            errorSpan.textContent = 'Error';
+            forecastDiv.appendChild(errorSpan);
+        }
+
+    }
+    function displayTodaysData(object) {
+        forecastDiv.style.display = 'block';
+
+        todaysDiv.appendChild(createTag('span', symbols[object.forecast.condition], 'condition symbol'));
+
+        let dataSpan = createTag('span', null, 'condition');
+        dataSpan.appendChild(createTag('span', object.name, 'forecast-data'));
+        dataSpan.appendChild(createTag('span', `${object.forecast.low}°/${object.forecast.high}°`, 'forecast-data'));
+        dataSpan.appendChild(createTag('span', `${object.forecast.condition}`, 'forecast-data'));
+
+        todaysDiv.appendChild(dataSpan);
+        currentDiv.appendChild(todaysDiv);
+
+    }
+
+    function displayUpcomingData(object) {
+
+        for (const day of object.forecast) {
+            let spanElement = createTag('span', null, 'upcoming');
+            spanElement.appendChild(createTag('span', symbols[day.condition], 'symbol'));
+            spanElement.appendChild(createTag('span', `${day.low}°/${day.high}°`, 'forecast-data'));
+            spanElement.appendChild(createTag('span', `${day.condition}`, 'forecast-data'));
+
+            threeDayForecast.appendChild(spanElement);
+        }
+
+        upcomingDiv.appendChild(threeDayForecast);
+    }
+    function createTag(tag, text = null, className = null) {
+        let el = document.createElement(tag);
+        if (text) { el.textContent = text; }
+        if (className) { el.className = className; }
+        return el;
+    }
 }
 
 attachEvents();
