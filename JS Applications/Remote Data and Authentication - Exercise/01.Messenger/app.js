d4b9df5354286af6c57e91801792b2bf6cd5f279
Polina <polina9130@gmail.com>
2022-11-16 09:49:40 +0200

    complete 01

--- JS Applications/Remote Data and Authentication - Exercise/01.Messenger/app.js
+++ JS Applications/Remote Data and Authentication - Exercise/01.Messenger/app.js
@@ -2,38 +2,43 @@ function attachEvents() {
     document.getElementById('submit').addEventListener('click', onPost);
     document.getElementById('refresh').addEventListener('click', getComments);
 
-    async function onPost() {
-        const name = document.getElementsByName('author').values[0];
-        const content = document.getElementsByName('content').values[0];
+    function onPost() {
+        const name = document.getElementsByName('author')[0];
+        const content = document.getElementsByName('content')[0];
 
-        await postComment({name, content});
+        const comment = {
+            author: name.value, 
+            content: content.value
+        };
+        name.value = "";
+        content.value = "";
+        postComment(comment);
     }
 }
 
 function reloadAll(data) {
-    const textarea = document.getElementById('messages')
+    const textarea = document.getElementById('messages');
     const datas = Object.values(data).map(entry => `${entry.author}: ${entry.content}`).join('\n');
     textarea.textContent = datas;
 }
 
 async function getComments() {
-    const response = await fetch('http://localhost:3030/jsonstore/messenger');
+    const response = await fetch("http://localhost:3030/jsonstore/messenger");
     const data = response.json();
 
     reloadAll(data);
 }
 
 async function postComment(comment) {
-    const response = await fetch('http://localhost:3030/jsonstore/messenger', {
-        method: 'post', 
+    const response = await fetch("http://localhost:3030/jsonstore/messenger", {
+        method: "post", 
         headers: {
-            'Content-Type': 'application/json'
+            "Content-Type": "application/json"
         },
         body: JSON.stringify(comment)
     });
 
-    const data = await response.json();
-    return data;
+    getComments();
 
 }
 
